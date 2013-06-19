3047f10ed6eae9c73203121a73f775772e5ac9d4
Jose Antonio Yañez Jimenez <jose@jimenezfrontend.es>
2013-06-19 14:19:40 +0200

    cambios en cron job

--- source/savexml.php
+++ source/savexml.php
@@ -12,24 +12,30 @@
 //////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////
 header('Access-Control-Allow-Origin: *');
-header('Content-type: application/xml');
 // Location of XML content
 $url = 'http://www.datosabiertos.jcyl.es/web/jcyl/risp/es/directorio/AlberguesJuveniles/1284198756067.xml';
 $pointer = fopen($url, 'r');
-$filename = "db/inns.xml";
+$tempname = "/home/u446842170/public_html/db/inns.xml.tmp";
+$filename = "/home/u446842170/public_html/db/inns.xml";
 // If the file/content exists, loop through the file until end of file
 if ($pointer)
 	{
-	$handle = fopen($filename, "a+");
+	$handle = fopen($tempname, "a+");
 	ftruncate($handle,0);
 	fclose($handle);
 	while (!feof($pointer)) 
 		{
 		$line = fgets($pointer); // Get data from current line
-		$handle = fopen($filename, "a+");
+		$handle = fopen($tempname, "a+");
 		fwrite($handle,$line);
 		fclose($handle);
 		}
 	fclose($pointer); // Close connection to file
+	if (!copy($tempname, $filename)) {
+    echo "Fallo en $filename...\n";
 	}
+	else
+	{echo("Correcto");}
+	}
+
 ?>
