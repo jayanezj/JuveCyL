9b68e6a375e88c1517756adb59fc63a9f0ef0e5c
Jose Antonio Yañez Jimenez <jose@jimenezfrontend.es>
2013-06-23 05:01:28 +0200

    Mas o menos están implementados los resultados

--- source/js/main.js
+++ source/js/main.js
@@ -8,17 +8,8 @@
 ////// 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function showInn() 
-	{
-	////////////////////////////////////////////////
-	////////////////////////////////////////////////
-	////   MUESTRA UN ALOJAMIENTO EN CONCRETO   ////
-	////////////////////////////////////////////////
-	////////////////////////////////////////////////
-	$('.linked').click(function (evt) 
-		{
-		evt.preventDefault();
-		var id = evt.target.id;
+function getInn(id)
+{
 		$.ajax(
 			{
 			type: "GET",
@@ -30,62 +21,190 @@ function showInn()
 				{
 				if($(this).find('attribute[name="Identificador"]').text() == id) 
 					{
+					$('.infotop').remove();
+					$('.infodown').remove();
 					////////////////////////////////////////////////
-					//DIV DESTINO QUE SOBRESALE
+					//DIV DESTINO SUPERIOR
 					////////////////////////////////////////////////
-					$('<div id="showinfo"></div>').html('').appendTo('body');
+					$('<div class="infotop"></div>').html('').appendTo('body');
+					////////////////////////////////////////////////
+					//CIERRE
+					////////////////////////////////////////////////
+					$('<p id="closeinfo"></p>').html('<img id="closedetails" src="img/cerrar.png" class="fade" alt="cerrar"></img>').appendTo('.infotop');
+					    $('#closedetails').bind({
+					        click: function (evt) {
+					            $('.infodown').fadeOut(800, function () {
+					                $('.infodown').remove()
+					            });
+					            $('.infotop').fadeOut(800, function () {
+					                $('.infotop').remove()
+					            });
+					        },
+			                mouseenter: function () {
+			                    $(this).fadeOut("fast");
+			                    $(this).attr("src", "img/cerrarhover.png");
+			                    $(this).fadeIn("fast");
+			                },
+			                mouseleave: function () {
+			                    $(this).fadeOut("fast");
+			                    $(this).attr("src", "img/cerrar.png");
+			                    $(this).fadeIn("fast");
+                			}
+					    });
 					////////////////////////////////////////////////
 					// INFORMACIÓN DEL ALOJAMIENTO
 					////////////////////////////////////////////////
+					//FOTO DEL ALBERGUE
+					$('.infotop').append('<img src="'+$(this).find('attribute[name="Imagen"]').find('link').find('reference').text()+'" alt="foto del albergue" id="picture">');
 					// TÍTULO
-					$('<p></p>').html($(this).find('attribute[name="Titulo_es"]').find('string').text()).appendTo('#showinfo');
-					// DESCRIPCIÓN
-					$('<p></p>').html($(this).find('attribute[name="Descripcion_es"]').find('text').text()).appendTo('#showinfo');
-					// TEMÁTICA
-					$('<p id="tem"></p>').html('').appendTo('#showinfo');
-						$('<ul id="tem_list"></ul>').html('').appendTo('#tem');
-						$(this).find('attribute[name="Tematica"]').find('array').find('string').each(function (list) 
-							{
-							$('<li></li>').html($(this).text()).appendTo('#tem_list');
-							});
+					$('<h1 id="infoname"></h1>').html($(this).find('attribute[name="Titulo_es"]').find('string').text()).appendTo('.infotop');
+					// DIRECCIÓN
+					$('<p id="address"></p>').html($(this).find('attribute[name="Localizacion"]').find('text').text()).appendTo('.infotop');
+					////////////////////////////////////////////////
+					// CONTACTO
+					////////////////////////////////////////////////
+					$('.infotop').append('<div class="contact"></div>');
 					// TELÉFONOS
-					$('<p id="phone"></p>').html('').appendTo('#showinfo');
-						$('<ul id="phone_list"></ul>').html('').appendTo('#phone');
-						$(this).find('attribute[name="Telefono"]').find('array').find('string').each(function () 
-							{
-							$('<li></li>').html($(this).text()).appendTo('#phone_list');
-							});
-					// FAXES
-					$('<p id="fax"></p>').html('').appendTo('#showinfo');
-						$('<ul id="fax_list"></ul>').html('').appendTo('#fax');
-						$(this).find('attribute[name="Fax"]').find('array').find('string').each(function () 
-							{
-							$('<li></li>').html($(this).text()).appendTo('#fax_list');
-							});
-					// EMAILS
-					$('<p id="mail"></p>').html('').appendTo('#showinfo');
-						$('<ul id="mail_list"></ul>').html('').appendTo('#mail');
-						$(this).find('attribute[name="Email"]').find('array').find('string').each(function () 
-							{
-							$('<li></li>').html($(this).text()).appendTo('#mail_list');
-							});
-					// WEBS
-					$('<p id="web"></p>').html('').appendTo('#showinfo');
-						$('<ul id="web_list"></ul>').html('').appendTo('#web');
-						$(this).find('attribute[name="Web"]').find('array').find('string').each(function () 
-							{
-							$('<li></li>').html('<a href="http://'+$(this).text()+'" target="_blank">'+$(this).text()+'</a>').appendTo('#web_list');
-							});
+					var phones="";
+					$(this).find('attribute[name="Telefono"]').find('array').find('string').each(function () 
+						{
+						phones+=$(this).text()+" /";
+						});
+					phones=phones.substring(0,phones.length-1);
+					$('<p id="phone"></p>').html('<span><img src="img/tfno.png" alt="tfno" /></span>'+phones).appendTo('.contact');
+					// FAX
+					var faxes="";
+					$(this).find('attribute[name="Fax"]').find('array').find('string').each(function () 
+						{
+						faxes+=$(this).text()+" /";
+						});
+					faxes=faxes.substring(0,faxes.length-1);
+					$('<p id="fax"></p>').html('<span><img src="img/fax.png" alt="fax" /></span>'+faxes).appendTo('.contact');
+					// MAIL
+					var mails="";
+					$(this).find('attribute[name="Email"]').find('array').find('string').each(function () 
+						{
+						mails+='<a href="mailto:'+$(this).text()+'">'+$(this).text()+'</a> /';
+						});
+					mails=mails.substring(0,mails.length-1);
+					$('<p id="mail"></p>').html('<span><img src="img/mail.png" alt="eMail" /></span>'+mails).appendTo('.contact');
+					// WEB
+					var webs="";
+					$(this).find('attribute[name="Web"]').find('array').find('string').each(function () 
+						{
+						webs+='<a href="http://'+$(this).text()+'" target="_blank">'+$(this).text()+'</a> /';
+						});
+					webs=webs+' <a target="_blank" href="http://192.168.100.150/juvenil/?id='+id+'">Enlace permanente</a>';
+					//webs+='<a href="http://www.juvecyl.tuars.com/?id='+id+'>Enlace permanente</a>';
+					$('<p id="web"></p>').html('<span><img src="img/web.png" alt="Webs" /></span>'+webs).appendTo('.contact');
+					////////////////////////////////////////////////
+					//NAVEGADOR ENLACES
+					////////////////////////////////////////////////
+					$('<nav id="navigation"></nav>').html('').appendTo('.infotop');
+					$('<ul></ul>').html('<li><img class="navlink" id="nav_activity" src="img/boton_actividades.png" alt="actividades" title="Actividades" /></li><li><img class="navlink" id="nav_howtoget" src="img/boton_como_llegar.png" alt="como llegar" title="Cómo llegar" /></li><li><img class="navlink" id="nav_description" src="img/boton_descripcion.png" alt="descripción" title="Descripción" /></li><li><img class="navlink" id="nav_equipment" src="img/boton_equipamiento.png" alt="equipamiento" title="Equipamiento" /></li><li><img class="navlink" id="nav_services" src="img/boton_servicios.png" alt="servicios" title="Servicios" /></li>').appendTo('#navigation');
+					$('.navlink').bind({
+						click: function (evt) {
+							$('#sec_description').fadeOut(500);
+							$('#sec_activity').fadeOut(500);
+							$('#sec_howtoget').fadeOut(500);
+							$('#sec_equipment').fadeOut(500);
+							$('#sec_services').fadeOut(500);
+							switch($(this).attr("id"))
+								{
+								case "nav_activity":
+								$('#sec_activity').delay(505).fadeIn(500);
+								break;
+								case "nav_description":
+								$('#sec_description').delay(505).fadeIn(500);
+								break;
+								case "nav_howtoget":
+								$('#sec_howtoget').delay(505).fadeIn(500);
+								break;
+								case "nav_equipment":
+								$('#sec_equipment').delay(505).fadeIn(500);
+								break;
+								case "nav_services":
+								$('#sec_services').delay(505).fadeIn(500);
+								break;
+								}
+					        },
+					    mouseenter: function (evt) {
+					    	$('.info_nav').fadeIn(200);
+					    	$('.info_nav').html($(this).attr("title"));
+					    	$('.info_nav').css('left',evt.clientX-30);
+					    	$('.info_nav').css('top',evt.clientY+40);
+					    },
+					    mouseleave: function (evt) {
+					    	$('.info_nav').fadeOut(200);
+					    }
+					});
+					////////////////////////////////////////////////
+					//DIV DESTINO INFERIOR
+					////////////////////////////////////////////////
+					$('<div class="infodown"></div>').html('').appendTo('body');
+					// DESCRIPCIÓN
+					$('<section id="sec_description"></section>').html('<h3>Descripción</h3><p>'+$(this).find('attribute[name="Descripcion_es"]').find('text').text()+'</p>').appendTo('.infodown');
+					// ACTIVIDAD
+					var act='<ul>';
+					$(this).find('attribute[name="Actividades"]').find('array').find('ActividadesAlojamiento').each(function () 
+						{
+						act+='<li>'+$(this).text()+'</li>';
+						});
+					act+='</ul>';
+					$('<section id="sec_activity"></section>').html('<h3>Actividades</h3><p>'+act+'</p>').appendTo('.infodown');
+					$('#sec_activity').hide();
+					// COMO LLEGAR
+					$('<section id="sec_howtoget"></section>').html('<h3>Cómo llegar</h3><p>'+$(this).find('attribute[name="ComoLlegar"]').find('text').text()+'</p>').appendTo('.infodown');
+					$('#sec_howtoget').hide();
+					// EQUIPAMIENTO
+					var eq='<ul>';
+					$(this).find('attribute[name="Equipamiento"]').find('array').find('Equipamiento').each(function () 
+						{
+						eq+='<li>'+$(this).text()+'</li>';
+						});
+					eq+='</ul>';
+					$('<section id="sec_equipment"></section>').html('<h3>Equipamiento</h3><p>'+eq+'</p>').appendTo('.infodown');
+					$('#sec_equipment').hide();
 					// SERVICIOS
-					$('<p id="services"></p>').html('').appendTo('#showinfo');
-						$('<ul id="services_list"></ul>').html('').appendTo('#services');
-						$(this).find('attribute[name="Servicios"]').find('array').find('Servicios').each(function () 
-							{
-							$('<li></li>').html($(this).text()).appendTo('#services_list');
-							});
+					var serv='<ul>';
+					$(this).find('attribute[name="Equipamiento"]').find('array').find('Equipamiento').each(function () 
+						{
+						serv+='<li>'+$(this).text()+'</li>';
+						});
+					serv+='</ul>';
+					$('<section id="sec_services"></section>').html('<h3>Servicios</h3><p>'+serv+'</p>').appendTo('.infodown');
+					$('#sec_services').hide();
+			        $('.infotop').fadeIn(800);
+			        $('.infodown').fadeIn(800);
+			        $('.infotop').bind("click", function (e) {
+        			e.stopPropagation();
+    				});
+			        $('.infodown').bind("click", function (e) {
+        			e.stopPropagation();
+    				});
+			        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+			        	$('.infotop').css('left','5px');
+			        	$('.infodown').css('left','5px');
+			        	$('.infotop').css('width',$(window).width-10+'px');
+			        	$('.infodown').css('width',$(window).width-10+'px');
+			        	}
+			        return false;
 					}
 				});
 			}});
+			}
+function showInn() 
+	{
+	////////////////////////////////////////////////
+	////////////////////////////////////////////////
+	////   MUESTRA UN ALOJAMIENTO EN CONCRETO   ////
+	////////////////////////////////////////////////
+	////////////////////////////////////////////////
+	$('.linked').click(function (evt) 
+		{
+		evt.preventDefault();
+		var id = evt.target.id;
+		getInn(id);
 		});
 	}
 function searchProvinces()
@@ -300,10 +419,31 @@ function loadResult()
 			});
 		}});
 	}
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
 $(document).ready(function () 
 	{
+    $('body').click(function () {
+        if($('.infodown').length) {
+            $('.infodown').fadeOut(800, function () {
+                $('.infodown').remove()
+            });
+            $('.infotop').fadeOut(800, function () {
+                $('.infotop').remove()
+            });
+        }
+    });
+	var res=getParameterByName('id');
 	//loadResult();
 	$('img[usemap]').rwdImageMaps();
 	searchInnByKey();
+    if (res!=null)
+    {
+   	getInn(res);
+    }
 	});
 
